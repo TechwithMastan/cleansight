ee49d3c0eac6c7f4c73ead282277163f62096b28
Mastan <99204265+TechwithMastan@users.noreply.github.com>
2024-01-09 21:48:28 +0530

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -74,22 +74,7 @@ include('server.php');
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
         }
 
-        @media screen and (min-width: 992px) {
-            .form-wrapper {
-                margin-top: 80px;
-            }
-        }
-
-        @media screen and (max-width: 767px) {
-            .form-wrapper {
-                padding: 20px;
-            }
-        }
-
-        @media screen and (max-width: 767px) {
-            #navbarNav .navbar-toggler-icon {
-                color: #000 !important;
-            }
+      
         }
 
         #header-1 {
